39236d03f07736c179797d7724f0ee18deba4b88
guopeng li <guopeng.li@technicolor.com>
2020-10-08 10:05:20 +0200

    add linear layout as a list of imgbtn for the corlor selection

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -3,15 +3,26 @@ package com.example.kidsdrawingapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var mImageButtonCurrentPaint: ImageButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawing_view.setSizeForBrush(10f)
 
-        //drawing_view.setSizeForBrush(20f)
+        mImageButtonCurrentPaint = ll_paint_colors[1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+        )
 
         ib_brush.setOnClickListener{
             showBrushSizeChooserDialog()
